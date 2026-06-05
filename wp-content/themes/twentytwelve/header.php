de4709aee80c3bb38547c829d5e8b0de4f25a962
Denis <dominov.denis@gmail.com>
2026-06-05 18:05:31 +0000

    Static mirror of auto-vip-transfer.com.ua (8 pages + assets, relative links)

---
+++ wp-content/themes/twentytwelve/header.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" href="mobile.css" type="text/css" media="screen"/>
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header" role="banner">
+	
+       <hgroup id="hgr">
+		<div class="cars2"><div class="logo-b"></div></div><div id="cars"><div id="vip"></div> <div class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"></a></div>
+		
+           <div class="cont_info">
+            	Украина, Одесса, ул.Пушкинская, 15<br>
+                тел.: +38 (048) 743 83 02<br>
+                моб.: +38 (098) 981 74 34<br>
+                моб.: +38 (093) 843 77 37<br>
+                e-mail: vyacheslav.fokin.26@gmail.com<br>
+            </div> </div> 
+		
+		</hgroup>
+        
+       <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+    	<div class="languages">
+    		<?php dynamic_sidebar( 'sidebar-2' ); ?>
+    	</div><!-- .first -->
+    	<?php endif; ?>
+        
+		<?php $header_image = get_header_image();
+		if ( ! empty( $header_image ) ) : ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+		<?php endif; ?>
+	</header><!-- #masthead -->
+	<input id="menu-checkbox" type="checkbox">
+    <nav id="site-navigation" class="main-navigation" role="navigation"><label id="toggle" class="toggle-button" onclick="" data-close="Закрыть меню" data-open="Открыть меню" for="menu-checkbox"></label>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+    </nav><!-- #site-navigation -->
+
+	<div id="main" class="wrapper">
